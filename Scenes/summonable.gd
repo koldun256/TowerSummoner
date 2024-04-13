f075f133c4f6eb966c41e2c3fb063b2ac3fd3310
koldun <eg.sharypov@ya.ru>
2024-04-13 20:21:40 +0300

    added summoning mechanic

--- Scenes/summonable.gd
+++ Scenes/summonable.gd
@@ -1,12 +1,13 @@
 extends Node
 
-@onready var player = get_tree().get_nodes_in_group("Player")[0]
+@onready var player = get_tree().get_first_node_in_group("Player")
 
 func summon():
-	print("summoning")
-	pass
+	print("clicked " + get_parent().name)
+	if player.close_tower != null:
+		print("...and player is close to a tower")
+		get_parent().global_position = player.close_tower.global_position
 
 func _on_area_2d_input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton and event.pressed:
-		#print("processing unit input")
 		summon()
