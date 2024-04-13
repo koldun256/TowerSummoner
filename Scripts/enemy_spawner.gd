857869d8d1c2dd058841dd68954bbd6eab86f594
Lazurit216 <lazurit216@gmail.com>
2024-04-13 17:50:00 +0300

    спавнер чиню

--- Scripts/enemy_spawner.gd
+++ Scripts/enemy_spawner.gd
@@ -1,14 +1,21 @@
 extends Node2D
 
-@export var enemy_node_path : PackedScene
+@export var enemy_node: PackedScene
 
-#@onready var enemy_node = get_node(enemy_node_path)
+# Периодичность спавна в секундах
+@export var spawn_interval: float = 2.0
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	# Запускаем таймер
+	$Timer.start(spawn_interval)
 
+# Функция вызывается при срабатывании таймера
+func _on_Timer_timeout():
+	# Создаем новую ноду на основе префаба
+	var new_enemy = enemy_node.instance()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+	# Добавляем созданную ноду на сцену
+	add_child(new_enemy)
+
+	# Перезапускаем таймер для следующего спавна
+	$Timer.start(spawn_interval)
