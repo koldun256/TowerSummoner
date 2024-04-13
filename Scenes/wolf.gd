d1e88818e4331df388f7a0e8a6ebb4f02b27d1a1
koldun <eg.sharypov@ya.ru>
2024-04-13 17:31:24 +0300

    wolf defends only his tower

--- Scenes/wolf.gd
+++ Scenes/wolf.gd
@@ -1,26 +1,36 @@
 extends Sprite2D
 
-@onready var enemy_spawner = get_node("../EnemySpawner")
 var target = null
 @export var attack_range = 30.0
 @export var damage = 30
 @export var attack_interval = 0.8
 var attack_cd = 0
 @export var speed = 500
+var tower
+
+func _ready():
+	tower = get_node('../Tower')
 
 func take_damage(d):
 	$HPBar.take_damage(d)
-	
-func choose_target():
-	var nearest_distance = 99999
-	var nearest_node: Node2D = null
 
-	for node in enemy_spawner.get_children():
-		var distance = global_position.distance_to(node.global_position)
+func choose_target():
+	print("choosing target")
+	var nearest_distance = 99999
+	var nearest_enemy: Node2D = null
+
+	for enemy in get_tree().get_nodes_in_group("Enemy"):
+		if "Building" in enemy.target2attack.get_groups() and enemy.target2attack != tower:
+			continue
+			
+		if "Summon" in enemy.target2attack.get_groups() and enemy.target2attack.target != tower:
+			continue
+			
+		var distance = global_position.distance_to(enemy.global_position)
 		if distance < nearest_distance:
 			nearest_distance = distance
-			nearest_node = node
-	return nearest_node
+			nearest_enemy = enemy
+	return nearest_enemy
 
 func attack(target):
 	target.take_damage(damage)
