12ccbaf0e1e2346b59fee8ed9b931e330f52d514
Lazurit216 <lazurit216@gmail.com>
2024-04-13 17:16:28 +0300

    начал скрипт писать

---
+++ Scripts/enemy_spawner.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+@export var enemy_node_path : PackedScene
+
+#@onready var enemy_node = get_node(enemy_node_path)
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
