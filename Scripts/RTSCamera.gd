fb07c5585bc4839df2d1b1ef785c45b413822c57
koldun <eg.sharypov@yandex.ru>
2024-04-13 14:25:39 +0300

    camera movement

---
+++ Scripts/RTSCamera.gd
@@ -0,0 +1,16 @@
+extends Camera2D
+
+@export var speed = 500.0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var dir_x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	var dir_y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	position.x += delta * speed * dir_x
+	position.y += delta * speed * dir_y
+	pass
