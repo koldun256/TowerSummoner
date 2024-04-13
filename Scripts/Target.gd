1c75c38a44f667a464ee5bd111d7e8b6725aa31d
Lazurit216 <lazurit216@gmail.com>
2024-04-13 21:34:34 +0300

    картинки

--- Scripts/Target.gd
+++ Scripts/Target.gd
@@ -2,6 +2,10 @@ extends Sprite2D
 
 func take_damage(d):
 	$HPBar.take_damage(d)
+	
+func die():
+	print("oh no im dead")
+	queue_free()
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	add_to_group("Targets")
