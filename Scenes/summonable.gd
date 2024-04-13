04968a08548b3dd03739a1ee8a503089c18600a2
koldun <eg.sharypov@ya.ru>
2024-04-13 20:24:35 +0300

    unsetting target on summon

--- Scenes/summonable.gd
+++ Scenes/summonable.gd
@@ -3,10 +3,10 @@ extends Node
 @onready var player = get_tree().get_first_node_in_group("Player")
 
 func summon():
-	print("clicked " + get_parent().name)
 	if player.close_tower != null:
-		print("...and player is close to a tower")
 		get_parent().global_position = player.close_tower.global_position
+		player.unset_target()
+		
 
 func _on_area_2d_input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton and event.pressed:
